c8a79843f603951921cfcce31ce11c253d6a471f
alexsincai <alex.sincai@yahoo.co.uk>
2021-10-18 23:50:19 +0300

    docstrings and linting

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+"""Scrapes https://biblior.net and outputs a sorted dictionary of letter frequencies."""
+
+from string import whitespace, punctuation, digits
+from typing import Any, List
 import requests
 from bs4 import BeautifulSoup
-from string import whitespace, punctuation, digits
 
 URL = "https://biblior.net"
 
 
-def read_links_on_page(url, selector):
+def read_links_on_page(url: str, selector: str) -> List[str]:
+    """Reads all links on a given page
+
+    Args:
+        url (str): The URL to be read
+        selector (str): The CSS selector for the wanted links
+
+    Returns:
+        List[str]: The desired list of links
+    """
     page = requests.get(url)
     tree = BeautifulSoup(page.text, "html.parser")
 
     return [item.attrs.get("href") for item in tree.select(selector)]
 
 
-def read_contents(url, body, removes):
+def read_contents(url: str, body: str, removes: List[str]) -> str:
+    """Reads the contents of a URL, keeping the element specified and removing the rest
+
+    Args:
+        url (str): The URL of the page to be read
+        body (str): The CSS selector for the content block
+        removes (List[str]): The list of CSS selectors to be removed from the page
+
+    Returns:
+        str: The text of the page
+    """
     page = requests.get(url)
     tree = BeautifulSoup(page.text, "html.parser")
 
@@ -28,7 +50,15 @@ def read_contents(url, body, removes):
     return output
 
 
-def filter_output(string):
+def filter_output(string: str) -> List[str]:
+    """Keeps only alphabetic characters from a string
+
+    Args:
+        string (str): The input string
+
+    Returns:
+        List[str]: The list of alphabetic characters
+    """
     extras = [
         "—",
         " ",
@@ -48,39 +78,59 @@ def filter_output(string):
         "½",
         "˝",
     ]
+
     return [
-        c.lower()
-        for c in string
-        if c not in whitespace
-        and c not in punctuation
-        and c not in digits
-        and c not in extras
+        character.lower()
+        for character in string
+        if character not in whitespace
+        and character not in punctuation
+        and character not in digits
+        and character not in extras
     ]
 
 
-def counter(array):
-    temp = {}
+def counter(array: List[str]) -> dict:
+    """Counts the number of times a character shows up in a list
 
-    for c in array:
+    Args:
+        array (List[str]): A list of characters
+
+    Returns:
+        dict: A dictionary of { character : number of occurences }
+    """
+    output = {}
+
+    for character in array:
         try:
-            temp[c] += 1
+            output[character] += 1
         except KeyError:
-            temp[c] = 1
+            output[character] = 1
 
-    return temp
+    return output
 
 
-def flatten(nested):
+def flatten(nested: List[List[Any]]) -> List[Any]:
+    """Flattens a nested list
+
+    Args:
+        nested (List[List[Any]]): A nested list
+
+    Returns:
+        List[Any]: A flattened list
+    """
     return [item for subl in nested for item in subl]
 
 
 def main():
+    """The main runner"""
 
+    # The main index of works
     page_links = read_links_on_page(
         url=URL + "/carti",
         selector=".views-summary-unformatted a",
     )
 
+    # The list of works for each letter
     links = flatten(
         [
             read_links_on_page(url=URL + item, selector="td.views-field a")
@@ -88,6 +138,7 @@ def main():
         ]
     )
 
+    # The list of pages for each work
     pages = flatten(
         [
             read_links_on_page(url=URL + item, selector="#content ul.menu li.leaf a")
@@ -95,6 +146,7 @@ def main():
         ]
     )
 
+    # This will be the output
     output = {}
 
     for page in pages:
@@ -107,12 +159,13 @@ def main():
         )
 
         count = counter(text)
-        for c in count:
+        for key in count:
             try:
-                output[c] += count[c]
+                output[key] += count[key]
             except KeyError:
-                output[c] = count[c]
+                output[key] = count[key]
 
+    # Sort the output by values rather than key
     output = dict(
         sorted(
             output.items(),
@@ -120,6 +173,8 @@ def main():
             reverse=True,
         )
     )
+
+    # The program's output
     print(output)
 
 
